bdf196c4082b73b03c3d3c6e5199de4641956fe0
Rafi_kabir <fab5.kk.1105@gmail.com>
2025-11-09 13:56:38 +0600

    add admin panel

--- Shokkhom-client-side/src/App.jsx
+++ Shokkhom-client-side/src/App.jsx
@@ -13,7 +13,7 @@ function App() {
       <Route path="/property" element={<Property />} />
       <Route path="/about" element={<About />} />
       <Route path="/contact" element={<Contact />} />
-      {/* <Route path="/admin" element={<AdminDashboard />} /> */}
+      <Route path="/admin" element={<AdminDashboard />} />
     </Routes>
   );
 }
